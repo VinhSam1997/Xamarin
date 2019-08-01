6976f122345e5d15e375fc91193ab186640ca0d6
VinhSam1997 <53247348+VinhSam1997@users.noreply.github.com>
2019-08-02 00:37:31 +1000

    Add files via upload

---
+++ PlayPage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace FruitMatching
+{
+    [Activity(Label = "PlayPage")]
+    public class PlayPage : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Playpage);
+            string textname = Intent.GetStringExtra("name" ?? "Not valid");
+
+            var username = FindViewById<TextView>(Resource.Id.username);
+            var userscore = FindViewById<TextView>(Resource.Id.score);
+            var backbutton = FindViewById<Button>(Resource.Id.back);
+
+            username.Text = textname;
+            backbutton.Click += delegate
+            {
+                this.Finish();
+            };
+        }
+    }
+}
